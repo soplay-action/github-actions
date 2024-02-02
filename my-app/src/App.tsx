4297ac6487b003496c8a99acbf781a100184dcb2
SOPLAY <40691745+SOPLAY@users.noreply.github.com>
2024-02-03 02:21:00 +0900

    Update App.tsx

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -28,7 +28,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <p>
-          Learn GithubActions cicd2-vks4
+          Learn GithubActions cicd3-vks1
         </p>
       </div>
       <p className='read-the-docs'>
